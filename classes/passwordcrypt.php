e4395321ca91867c6b78be170ce02506b48cd086
matthewclayton <mattclayton1992@gmail.com>
2016-07-13 22:40:21 -0400

    Small changes

--- classes/passwordcrypt.php
+++ classes/passwordcrypt.php
@@ -6,7 +6,51 @@
  * Time: 9:58 PM
  */
 
+/**mod idea
+ * customize the cost factor from admin panel
+ * */
+
+
 class PasswordCrypt
 {
+    protected $hashOptions = array();
+
+    protected $passwordText;
+
+    protected $passwordHash;
+
+    public function __construct($userPassword)
+    {
+        $this->passwordText = $userPassword;
+    }
+
+    public function getCost()
+    {
+        return 9;
+    }
+
+    protected function setHashOptions()
+    {
+        $this->hashOptions['cost'] = $this->getCost();
+    }
+
+    protected function getHashOptions()
+    {
+        return $this->hashOptions;
+    }
+
+    protected function setHash()
+    {
+        $this->passwordHash = password_hash($this->passwordText, PASSWORD_BCRYPT, $this->getHashOptions());
+    }
+
+    protected function getHash()
+    {
+        return $this->passwordHash;
+    }
+
 
 }
+
+
+?>
