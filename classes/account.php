0a19545ac84f3c52d4c5335abc78ed27fceee2e6
matthewclayton <mattclayton1992@gmail.com>
2016-07-20 19:26:49 -0400

    Debugg

--- classes/account.php
+++ classes/account.php
@@ -19,7 +19,7 @@ class Account
     public function getExists($field, $value)
     {
         $queryArray = array(
-            "$field" => $value,
+            'username' => $value,
         );
         $this->database->setTableName('user_accounts');
         $this->database->setQueryData($queryArray);
